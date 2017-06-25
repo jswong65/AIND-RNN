238725e4de332eed0bc86fb77d631744f68d1dba
jswong65 <jswong65@gmail.com>
2017-06-24 21:43:13 -0400

    Implment todo functions

--- my_answers.py
+++ my_answers.py
@@ -10,9 +10,13 @@ import keras
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series,window_size):
     # containers for input/output pairs
+    n_nums = len(series)
     X = []
     y = []
-
+    for i in range(n_nums - window_size):
+        X.append(list(series[i:i+window_size]))
+        y.append(series[i+window_size])
+        
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,16 +27,40 @@ def window_transform_series(series,window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
-    pass
+    # given - fix random seed - so we can all reproduce the same results on our default time series
+    np.random.seed(0)
 
+
+    # TODO: build an RNN to perform regression on our time series input/output data
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size,1)))
+    model.add(Dense(1))
+
+
+    # build model using keras documentation recommended optimizer initialization
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+    # compile the model
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+    return model
 
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
     # find all unique characters in the text
+    import string
 
+    printable = set(string.printable)
+    printable.remove('\t')
+    printable.remove('\n')
+    printable.remove('\r')
+    printable.remove('\x0b')
+    printable.remove('\x0c')
 
     # remove as many non-english characters and character sequences as you can 
-
+    filter(lambda ch: ch in printable, text)
+    
+    # shorten any extra dead space created above
+    text = text.replace('  ',' ')
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text,window_size,step_size):
@@ -40,6 +68,9 @@ def window_transform_text(text,window_size,step_size):
     inputs = []
     outputs = []
     
-
-    
+    n_chars = len(text)
+    for i in range(0, n_chars - window_size, step_size):
+        inputs.append(text[i:i+window_size])
+        outputs.append(text[i+window_size])
+        
     return inputs,outputs
